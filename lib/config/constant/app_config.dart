c1396780dbe57175764af6386c74f6db3960dafc
Ynhaein <ynhaein@gmail.com>
2026-05-16 23:08:45 +0800

    api key configurasi

--- lib/config/constant/app_config.dart
+++ lib/config/constant/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
   static const String baseUrl = 'https://api.themoviedb.org/3';
-  static const String apiKey = '17c7d48688f51e7f56d5af10dfedf771';
+  static const String apiKey = 'Your Api Key';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p';
 }
